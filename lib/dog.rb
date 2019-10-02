acd152ae41334ccde89289a88a9369c55a20321e
Naveed Ganjani <nganjani@protonmail.com>
2019-10-02 20:57:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,3 +17,23 @@ class Dog
   end
 
 end
+
+class Person
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+end
